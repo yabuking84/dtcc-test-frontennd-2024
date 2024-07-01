aad60436254b76f1e908a398d7463a25e6489d38
Tawing <yabuking84@gmail.com>
2024-07-01 10:18:27 +0400

    refactor

--- src/hooks/useCitizen.ts
+++ src/hooks/useCitizen.ts
@@ -25,195 +25,193 @@ export const useCitizen = () => {
         'idle' | 'adding' | 'success' | 'fail'
     >('idle')
 
-    const addCitizen = async (payload: CitizenFormType) => {
-        CitizenFormSchema.parse(payload)
-
-        if (!wCtx.account) {
-            toast({
-                title: 'Error',
-                description: 'Please connect to your wallet',
-                variant: 'destructive',
+    const action = {
+        initCitizens: async function (block: Block) {
+            const events = await wCtx.contract?.getPastEvents('allEvents', {
+                fromBlock: block.fromBlock,
+                toBlock: block.toBlock,
             })
-            return
-        }
 
-        setIsAdding(true)
-        setAddingStatus('adding')
-        try {
-            await wCtx.contract?.methods
-                ?.addCitizen(
-                    payload.age,
-                    payload.city,
-                    payload.name,
-                    payload.someNote,
-                )
-                .send({
-                    from: wCtx.account,
-                    gas: '3000000',
-                })
-                .on('receipt', () => {
-                    toast({
-                        title: 'Success',
-                        description: 'Citizen added!',
-                    })
-                    setIsAdding(false)
-                    setAddingStatus('idle')
-                })
-                .on('error', () => {
-                    throw new Error('Error')
-                })
-        } catch (error) {
-            console.error(error)
-            toast({
-                title: 'Error',
-                description: 'Error adding citizen',
-                variant: 'destructive',
-            })
-            throw new Error('Error')
-        } finally {
-            setIsAdding(false)
-            setAddingStatus('idle')
-        }
-    }
+            if (events?.length) {
+                /**
+                 * Had difficulty decoding city to string,
+                 * so had to redeploy the contract with no indexed city :-(
+                 */
+                const iface = new ethers.Interface([
+                    'event Citizen(uint indexed id, uint indexed age, string city, string name)',
+                ])
+                const ctzns: CitizenType[] = []
+                for (let i = 0; i < events.length; i++) {
+                    const e = events[i]
+                    if (typeof e !== 'string' && e.transactionHash) {
+                        const receipt =
+                            await wCtx.web3?.eth.getTransactionReceipt(
+                                e.transactionHash,
+                            )
+                        if (receipt) {
+                            // Extract logs
+                            const logs = receipt.logs
 
-    const getNoteById = async (id: string): Promise<string> => {
-        try {
-            setIsGettingNote(true)
-            const note = await wCtx.contract?.methods
-                .getNoteByCitizenId(id)
-                .call()
-            setIsGettingNote(false)
-            return note?.toString() || ''
-        } catch (error) {
-            setIsGettingNote(false)
-            toast({
-                title: 'Error',
-                description: 'Getting note failed.',
-                variant: 'destructive',
-            })
-            return ''
-        }
-    }
-
-    const getBlock = async (): Promise<Block> => {
-        const block = (await wCtx.web3?.eth.getBlockNumber()) || 1000001n
-        return {
-            fromBlock: block - 1000000n,
-            toBlock: block,
-        }
-    }
-
-    const initCitizens = async (block: Block) => {
-        const events = await wCtx.contract?.getPastEvents('allEvents', {
-            fromBlock: block.fromBlock,
-            toBlock: block.toBlock,
-        })
-
-        if (events?.length) {
-            /**
-             * Had difficulty decoding city to string, 
-             * so had to redeploy the contract with no indexed city :-( 
-             */
-            const iface = new ethers.Interface([
-                'event Citizen(uint indexed id, uint indexed age, string city, string name)',
-            ])
-            const ctzns: CitizenType[] = []
-            for (let i = 0; i < events.length; i++) {
-                const e = events[i]
-                if (typeof e !== 'string' && e.transactionHash) {
-                    const receipt = await wCtx.web3?.eth.getTransactionReceipt(
-                        e.transactionHash,
-                    )
-                    if (receipt) {
-                        // Extract logs
-                        const logs = receipt.logs
-
-                        // Decode logs
-                        logs.forEach((log) => {
-                            const _log = log as {
-                                topics: readonly string[]
-                                data: string
-                            }
-                            try {
-                                if (_log) {
-                                    const decodedLog = iface.parseLog(_log)
-                                    const buf = {
-                                        id: parseInt(decodedLog?.args[0]),
-                                        age:
-                                            parseInt(decodedLog?.args[1]) ||
-                                            0,
-                                        city: decodedLog?.args[2] || '',
-                                        name: decodedLog?.args[3] || '',
-                                    }
-                                    if (decodedLog?.args[0] && ctzns.findIndex(e=>e.id===buf.id) < 0 )
-                                        ctzns.push(buf)
+                            // Decode logs
+                            logs.forEach((log) => {
+                                const _log = log as {
+                                    topics: readonly string[]
+                                    data: string
                                 }
-                            } catch (e) {
-                                console.error(
-                                    'Log does not match the event signature:',
-                                    e,
-                                )
-                            }
-                        })
+                                try {
+                                    if (_log) {
+                                        const decodedLog = iface.parseLog(_log)
+                                        const buf = {
+                                            id: parseInt(decodedLog?.args[0]),
+                                            age:
+                                                parseInt(decodedLog?.args[1]) ||
+                                                0,
+                                            city: decodedLog?.args[2] || '',
+                                            name: decodedLog?.args[3] || '',
+                                        }
+                                        if (
+                                            decodedLog?.args[0] &&
+                                            ctzns.findIndex(
+                                                (e) => e.id === buf.id,
+                                            ) < 0
+                                        )
+                                            ctzns.push(buf)
+                                    }
+                                } catch (e) {
+                                    console.error(
+                                        'Log does not match the event signature:',
+                                        e,
+                                    )
+                                }
+                            })
+                        }
                     }
                 }
+
+                setCitizens(ctzns.reverse())
+            } else {
+                setCitizens([])
             }
-
-            setCitizens(ctzns.reverse())
-        } else {
-            setCitizens([])
-
-        }
-    }
-
-    const init = async () => {
-        setIsLoading(true)
-        try {
-            const block = await getBlock()
-            await initCitizens(block)
-            await waits(500) // so that it wont be too twitchy
-        } catch (error: any) {
-            console.error(error)
-            if (
-                (error.message + '').includes(
-                    'MetaMask is not connected/installed',
+        },
+        init: async function () {
+            setIsLoading(true)
+            try {
+                const block = await getter.getBlock()
+                await action.initCitizens(block)
+                await waits(500) // so that it wont be too twitchy
+            } catch (error: any) {
+                console.error(error)
+                if (
+                    (error.message + '').includes(
+                        'MetaMask is not connected/installed',
+                    )
                 )
-            )
-                toast({
-                    title: 'Wallet not Connected',
-                    description: 'Please connect to your wallet',
-                })
-            else
+                    toast({
+                        title: 'Wallet not Connected',
+                        description: 'Please connect to your wallet',
+                    })
+                else
+                    toast({
+                        title: 'Error',
+                        description: 'Error initializing Citizens.',
+                        variant: 'destructive',
+                    })
+            } finally {
+                setIsLoading(false)
+            }
+        },
+        addCitizen: async function (payload: CitizenFormType) {
+            CitizenFormSchema.parse(payload)
+
+            if (!wCtx.account) {
                 toast({
                     title: 'Error',
-                    description: 'Error initializing Citizens.',
+                    description: 'Please connect to your wallet',
                     variant: 'destructive',
                 })
-        } finally {
-            setIsLoading(false)
-        }
+                return
+            }
+
+            setIsAdding(true)
+            setAddingStatus('adding')
+            try {
+                await wCtx.contract?.methods
+                    ?.addCitizen(
+                        payload.age,
+                        payload.city,
+                        payload.name,
+                        payload.someNote,
+                    )
+                    .send({
+                        from: wCtx.account,
+                        gas: '3000000',
+                    })
+                    .on('receipt', () => {
+                        toast({
+                            title: 'Success',
+                            description: 'Citizen added!',
+                        })
+                        setIsAdding(false)
+                        setAddingStatus('idle')
+                    })
+                    .on('error', () => {
+                        throw new Error('Error')
+                    })
+            } catch (error) {
+                console.error(error)
+                toast({
+                    title: 'Error',
+                    description: 'Error adding citizen',
+                    variant: 'destructive',
+                })
+                throw new Error('Error')
+            } finally {
+                setIsAdding(false)
+                setAddingStatus('idle')
+            }
+        },
+        resetCitizens: function () {
+            setCitizens([])
+        },
+        paginateCitizens: function (pageSize: number, pageNumber: number) {
+            const startIndex = (pageNumber - 1) * pageSize
+            const endIndex = startIndex + pageSize
+            return citizens.slice(startIndex, endIndex)
+        },
     }
 
-    const resetCitizens = () => {
-        setCitizens([])
-    }
+    const getter = {
+        getNoteById: async function (id: string): Promise<string> {
+            try {
+                setIsGettingNote(true)
+                const note = await wCtx.contract?.methods
+                    .getNoteByCitizenId(id)
+                    .call()
+                setIsGettingNote(false)
+                return note?.toString() || ''
+            } catch (error) {
+                setIsGettingNote(false)
+                toast({
+                    title: 'Error',
+                    description: 'Getting note failed.',
+                    variant: 'destructive',
+                })
+                return ''
+            }
+        },
 
-    const paginateCitizens = (pageSize: number, pageNumber: number) => {
-        const startIndex = (pageNumber - 1) * pageSize
-        const endIndex = startIndex + pageSize
-        return citizens.slice(startIndex, endIndex)
+        getBlock: async function (): Promise<Block> {
+            const block = (await wCtx.web3?.eth.getBlockNumber()) || 1000001n
+            return {
+                fromBlock: block - 1000000n,
+                toBlock: block,
+            }
+        },
     }
 
     return {
-        action: {
-            init,
-            addCitizen,
-            resetCitizens,
-            paginateCitizens,
-        },
-        getter:{
-            getNoteById,
-        },
+        action,
+        getter,
         state: {
             citizens,
             isLoading,
